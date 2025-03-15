b281eff49fddbf645467371938b998f7f1a92841
DraizaClaw <zayood.asfour@icloud.com>
2025-03-15 16:37:44 +0300

    pathfollowing
    made enemy follow path

---
+++ ZombieTowerDefence/Assets/Scripts/EnemyMovemment.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class EnemyMovemment : MonoBehaviour
+{
+
+    [Header("Refrences")]
+ 
+    private Rigidbody2D rb;
+
+
+    [Header("Attributes")]
+    [SerializeField] private float speed;
+
+    private Transform target;
+    private int PathIndex;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        target = LevelManager.main.path[PathIndex];
+    }
+    private void Update()
+    {
+        if (Vector2.Distance(target.position,transform.position) <= 0.1f)
+        {
+            PathIndex++;
+
+
+            if (PathIndex==LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.path[PathIndex];
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.linearVelocity = direction * speed;
+    }
+}
